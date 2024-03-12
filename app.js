66d011c83020b360f701619e5f261f35ff1da28b
theharshal05 <minespace005@gmail.com>
2024-03-12 20:12:47 +0530

    Random_color_Genrator

---
+++ app.js
@@ -0,0 +1,20 @@
+const getColor = () =>{
+    //Hex code
+    const randomNumber =Math.floor  (Math.random() * 16777215)
+    const randomCode = "#" +randomNumber.toString(16);
+   document.body.style.backgroundColor = randomCode;
+   document.getElementById("color-code").innerText = randomCode;
+
+   navigator.clipboard.writeText(randomCode)
+}
+
+// event call
+
+document.getElementById("btn").addEventListener(
+    "click",
+    getColor
+)
+
+// Init call
+getColor();
+
